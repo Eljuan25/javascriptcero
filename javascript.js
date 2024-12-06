3a379fee9e7beaa61e6dec961243e6641c3f4289
juan25 <elbartoy@gmail.com>
2024-12-05 23:54:07 -0600

    Este es el primer hola mundo

---
+++ javascript.js
@@ -0,0 +1,34 @@
+
+
+//
+
+
+console.log ("*");
+console.log ("**");
+console.log ("***");
+console.log ("||");
+
+// let, const , var 
+
+//let
+let nombre = "Juan";
+
+
+
+//const
+const pi = 3.14;
+
+
+//Var
+if (true){
+    var global = "Esto var";
+    let local  = "Esto let";
+}
+
+console.log(global +  "esto es")
+console.log("Hola , como estas?  " + nombre);
+console.log("Este es el valor de pi " + pi);
+
+
+
+
